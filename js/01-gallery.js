3c2bc5851a138166e87a770d2d4fb3baa1152c0e
Vadim Korol <55877443+korolvv@users.noreply.github.com>
2023-05-21 20:16:20 +0300

    update files

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,62 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
+
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+function createItem() {
+	const itemArr = [];
+
+	galleryItems.forEach((item) => {
+		let li = document.createElement("li");
+		let link = document.createElement("a");
+        let img = document.createElement("img");
+        
+        li.classList.add("gallery__item");
+        
+		link.classList.add("gallery__link");
+        link.href = item.original;
+        
+		img.classList.add("gallery__image");
+		img.src = item.preview;
+        img.alt = item.description;
+        
+		link.appendChild(img);
+		li.appendChild(link);
+		itemArr.push(li);
+	});
+	gallery.append(...itemArr);
+}
+
+createItem();
+
+gallery.addEventListener("click", openFullSize);
+
+function openFullSize(e) {
+    const { dataset, alt } = e.target;
+	e.preventDefault();
+	const content = document.createElement("div");
+	const img = document.createElement("img");
+
+	img.src = dataset.source;
+	img.alt = alt;
+	content.appendChild(img);
+
+	const instance = basicLightbox.create(content);
+    
+    instance.show();
+    
+    document.addEventListener("keydown", (event) => {
+        if (event.key === 'Escape') {
+            const visible = basicLightbox.visible();
+            if (visible) {
+                instance.close();
+            }
+        }
+    });
+}
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// const gallery = new SimpleLightbox('.gallery a');
+const gallery = document.querySelector(".gallery");
+
+function createItem() {
+	const itemArr = [];
+
+	galleryItems.forEach((item) => {
+		let li = document.createElement("li");
+		let link = document.createElement("a");
+		let img = document.createElement("img");
+
+		li.classList.add("gallery__item");
+
+		link.classList.add("gallery__link");
+		link.href = item.original;
+
+		img.classList.add("gallery__image");
+		img.src = item.preview;
+		img.alt = item.description;
+
+		link.appendChild(img);
+		li.appendChild(link);
+		itemArr.push(li);
+	});
+
+	gallery.append(...itemArr);
+}
+
+createItem();
+
+const galleryList = new SimpleLightbox(".gallery a");
+
